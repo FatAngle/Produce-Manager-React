17d0953094ba6413931152d0e1fe15252fb78892
FatAngle <zhangxl_1302@163.com>
2023-04-24 23:17:04 +0800

    add a test html file

--- src/test.js
+++ src/test.js
@@ -1,4 +1,14 @@
-const test = () => {
-  console.log("test");
-};
-test();
+<!DOCTYPE html>
+<html>
+  <head>
+    <mate charset='utf-8'>
+      <titile>
+        xiaolei's title
+      </titile>
+  </head>
+  <body>
+    <h1>
+      This is my header 1.
+    </h1>
+  </body>
+</html>
